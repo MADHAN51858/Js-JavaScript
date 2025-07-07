445bad631974f65b63679066881725accacea151
Madhan <madhanku635@gmail.com>
2025-07-07 06:25:10 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+/* alert("hello")
+it's an brower method and can't used in node.js */
+
+console.log("Madhu") //code readability must be high
+
+let name = "Madhu" //string most of the times used inside " " and sometimes ' ' 
+let age = 20  //number range 2^53, beyound that bigInt is used
+let isLogged = true //booleean true/false
+
+// null => datatype along with it ,it's an standalone Value also
+
+// undefined => means value is not assigned yet 
+
+// symbol => used to find uniqueness ,mostly used in react and figma 
+
+//object 
+
+console.log(typeof name);
+console.log(typeof null); //type => object
+console.log(typeof undefined);// type => undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accId = 5226
+let accEmail = "madhu@5226.com"
+let accState
+
+/*var accPassword = "5226" 
+we don't use var because issues in the block and function scope 
+*/
+accCity = "Banglore" //variable can be declared like this also
+
+// accId = 2256 not allowed
+
+accEmail = "madhan@5226.com"
+accPassword = "555666"
+accCity = "Mysore"
+
+// console.log(accEmail);
+// console.log(accPassword);
+// console.log(accCity);
+
+console.table([accEmail, accPassword, accCity,accState])
