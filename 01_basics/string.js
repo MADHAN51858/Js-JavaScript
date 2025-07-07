81a595de43b92719150c782cbf23353c1aa4d0ac
Madhan <madhanku635@gmail.com>
2025-07-07 06:43:24 +0000

    type Conversion

---
+++ 01_basics/string.js
@@ -0,0 +1,43 @@
+
+
+// Type Conversion
+
+let score = "33"
+let score1 = "33adasdad"
+let number = Number(score)
+let number1 = Number(score1)
+
+// console.log(typeof score);
+// console.log(typeof(score)); Both are same
+
+// console.log(typeof number);
+
+/* it's says type is number , but 
+ when you actually print the value of it ,it says Not a Number
+ be Careful while using numbers
+ */
+
+// console.log(typeof number1); //number 
+// console.log(number1); //NaN
+
+/* Summery 
+"33" => 33
+"33abs" => NaN
+true => 1; false => 0
+*/
+
+// let isLogged = 1  => true
+// let isLogged = "Madhu"  => true
+// let isLogged  => false
+// let isLogged = ""  => false
+
+// let bollean = Boolean(isLogged)
+// console.log(bollean);
+
+
+let sumNumber = 33
+
+// let str = String(sumNumber)
+// console.log(typeof str);
+// console.log(str);
+
